4192e6c39f35f3c151f35ed52c6b4f3b4d4144de
Thomas Rabaix <thomas.rabaix@gmail.com>
2015-03-14 16:44:45 +0100

    fix file name

--- react-admin/rest/__tests__/EndPoint-test.js
+++ react-admin/rest/__tests__/EndPoint-test.js
@@ -1,9 +1,9 @@
-jest.dontMock('../Endpoint');
+jest.dontMock('../EndPoint');
 jest.dontMock('lodash');
 
 describe('Endpoint', function () {
     it('init Endpoint', function () {
-        var Endpoint = require('../Endpoint');
+        var Endpoint = require('../EndPoint');
 
         var e = new Endpoint('http://192.168.30.20:9090/nodes', {'Accept': 'application/json'});
 
